7c664d3c4b209a486024e917495e1a41df672d47
Abir-Hasan-GUB <abirhasan6111@gmail.com>
2020-08-25 13:46:38 +0600

    End Core concepts

--- src/App.js
+++ src/App.js
@@ -1,26 +1,106 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let nayok = ['abir','mim','nahid'];
+  let nayika = ['Srabonti','Porimony','Shabana'];
+  var parson = {
+    name: "Abir Hasan",
+    id: 181002002
+  };
+  let Style = {
+    color: 'red',
+    backgroundColor: 'yellow'
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+      <Counter></Counter>
+
+      <Users></Users>
+
+
+        {
+            nayok.map(nayok => <Person nayok = {nayok}></Person>)
+            // nayika.map(nayika => <Person nayika = {nayika}></Person>)
+        }
+        
+        <Person nayok="A" nayika = "Chaka khor"></Person>
+        <Person nayok = "Manna" nayika = "Purnima"></Person>
+        <Person nayok = "Eliyas Kancon" nayika = "Bobita"></Person> 
+        
+        <h1 style={Style}>Name: {parson.name}</h1>
+        <h2 style={{
+          color: 'cyan',
+          fontSize: '35px'
+        }}>ID: {parson.id}</h2>
+
+
+
+
+
       </header>
     </div>
   );
 }
 
+function Counter(){
+  const [count,setCount] = useState(1000);
+  const handleIncrase =() => {
+   const newCount = count - 1;
+   if(newCount <0 ){
+     setCount(2000000);
+   }else{
+     setCount(newCount);
+   }
+  };
+  return (
+    <div>
+      <h1>Count: {count}</h1>
+      <button onMouseMove={handleIncrase} style = {{padding:'10px',cursor:'pointer',fontSize:'20px'}}>Incress Count</button>
+    </div>
+  )
+}
+
+function Users(){
+  const [user,setUser] = useState([]);
+  useEffect(()=>{
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(data => setUser(data))
+  },[]) 
+  return(
+    <div>
+      <h2>Dynamic Users: {user.length}</h2>
+      <ol>
+        {
+          user.map(user => <li>{user.name}</li>)
+        }
+      </ol>
+    </div>
+  )
+}
+
+
+
+
+
+
+
+
+
+function Person(nayok) {
+  let style = {
+    border: '2px solid red', 
+    margin: '3px'
+  }
+  return (<div style={style}>
+    <h2>Nayok: {nayok.nayok}</h2>
+  {/* <h3>Nayika: {nayika.nayika}</h3> */}
+  </div>
+  )
+}
 export default App;
